a24a696155364313c893a2464b7029758865470f
Kinsammy <fanusamuel@gmail.com>
2024-01-18 11:14:07 +0100

    Let restart python journey

--- kuna_tutorial/number_reverse.py
+++ kuna_tutorial/number_reverse.py
@@ -1,12 +1,18 @@
 def reverse(number):
+    isNegative = False
+    if number < 0:
+        isNegative = True
+        number *= -1
     reverse_num = 0
     while number > 0:
         rem = number % 10
-        number //=10
+        number //= 10
         reverse_num = reverse_num * 10 + rem
-    print(reverse_num)
+    if isNegative:
+        reverse_num *= -1
+    return reverse_num
 
 
 if __name__ == '__main__':
-    num = 654321
-    reverse(num)
+    num = 1534236469
+    print(reverse(num))
