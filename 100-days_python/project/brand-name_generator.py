35d471e47b4cfc87d1550aaf09bb95878ef49166
Kinsammy <fanusamuel@gmail.com>
2024-01-18 17:33:39 +0100

    Brand name generator

---
+++ 100-days_python/project/brand-name_generator.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    city_name = str(input("What's name of the city you grew up in?\n"))
+    pet_name = str(input("What's your pet's name?\n"))
+
+    print("Your brand name could be", city_name + " " + pet_name)
